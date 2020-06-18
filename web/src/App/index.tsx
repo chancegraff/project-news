0ae0b90d8bd9fd4eff6c1c130ee03d46909f08b2
Chance Graff <cgraffrun@gmail.com>
2020-06-17 21:22:02 -0500

    Change articles url to generate url as a test

--- web/src/App/index.tsx
+++ web/src/App/index.tsx
@@ -56,7 +56,7 @@ const setLocalAuth = (hash: IAuth) => {
   }
 };
 
-const articlesURL = "/api/v1/collector/all";
+const articlesURL = "/api/v1/token/generate";
 const generateURL = "/api/v1/token/generate";
 
 const identifiers: IIdentifiers = {
@@ -119,8 +119,6 @@ const App = () => {
   useEffect(() => {
     const fn = async () => {
       if(auth && auth.hash && !articles.length) {
-        console.log('Effecting')
-        debugger;
         const articlesOption = {
           method: "POST",
           headers: {
@@ -131,7 +129,6 @@ const App = () => {
           }),
         };
         const rsp = await fetch(articlesURL, articlesOption);
-        debugger;
         const js = await rsp.json();
 
         setArticles(js.articles);
